ab92039975b9c238a107e0e47ce177818d8f401d
luckyharryji <xiangyu.ji@outlook.com>
2016-12-16 17:38:50 -0800

    parse PS for process content

--- code/user/ps.c
+++ code/user/ps.c
@@ -7,13 +7,36 @@
 int
 main(int argc, char *argv[])
 {
+  if(argc > 1){
+    printf(1, "usage: ps\n");
+    exit();
+  }
+
   struct ProcessInfo *procInfo = malloc(sizeof(struct ProcessInfo) * NPROC);
   int numProcs;
   
   numProcs = getprocs(procInfo);
 
-  printf(1, "There are total %d process\n", numProcs);
-  
+  if (numProcs < 0) {
+    printf(1, "error for ps command\n");
+    exit();
+  }
+
+  char states [6][16]= {
+    {"UNUSED"},
+    {"EMBRYO"}, 
+    {"SLEEPING"},
+    {"RUNNABLE"},
+    {"RUNNING"},
+    {"ZOMBIE"}
+  };
+
+  int i;
+  for (i = 0; i < numProcs; i++) {
+    printf(1, "%d  %d  %s  %d  %s\n", procInfo->pid, procInfo->ppid, states[procInfo->state],  procInfo->sz, procInfo->name);
+    procInfo += 1;
+  }
+
   free(procInfo);
   exit();
 }
